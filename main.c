deeac6a754771424f94e2e83a88bcc61ddb679d0
mhcrnl <mhcrnl@gmail.com>
2021-03-13 18:16:39 +0200

    Sat Mar 13 18:16:39 EET 2021

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 
 #include <gtk/gtk.h>
 
+const gchar *title = "Gtk3_2021_TextEditor";
+
 int main(int argc, char *argv[]) {
 	
 	GtkWidget *window;
@@ -8,6 +10,9 @@ int main(int argc, char *argv[]) {
 	gtk_init(&argc, &argv);
 	
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), title);
+	gtk_window_set_default_size(GTK_WINDOW(window), 540, 370);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	gtk_widget_show(window);
 	
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
